c5ce59e018ddc3fefb736aef69c2518ebd92a5ac
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-03-23 16:28:23 +0000

    feat: add GET, POST routes for create, edit, delete genres

--- routes/genresRouter.js
+++ routes/genresRouter.js
@@ -3,6 +3,12 @@ const genresRouter = Router();
 const genresControllers = require("../controllers/genresControllers");
 
 genresRouter.get("/", genresControllers.getAllGenres);
+genresRouter.get("/create", genresControllers.getCreateGenre);
 genresRouter.get("/:genreId", genresControllers.getGamesByGenreId);
+genresRouter.get("/edit/:genreId", genresControllers.getUpdateGenreById);
+
+genresRouter.post("/create", genresControllers.genreValidators, genresControllers.createGenre);
+genresRouter.post("/edit/:genreId", genresControllers.genreValidators, genresControllers.updateGenre);
+genresRouter.post("/delete/:genreId", genresControllers.deleteGenre);
 
 module.exports = genresRouter;
