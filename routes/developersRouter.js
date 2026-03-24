02c4e78f68c17cf82f9da37695eb61a34cb2c6b2
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-03-24 11:28:09 +0000

    feat: add GET, POST routes for create, edit, delete developers

--- routes/developersRouter.js
+++ routes/developersRouter.js
@@ -3,6 +3,12 @@ const developersRouter = Router();
 const developersControllers = require("../controllers/developersControllers");
 
 developersRouter.get("/", developersControllers.getAllDevelopers);
+developersRouter.get("/create", developersControllers.getCreateDeveloper);
 developersRouter.get("/:developerId", developersControllers.getGamesByDeveloperId);
+developersRouter.get("/edit/:developerId", developersControllers.getUpdateDeveloperById);
+
+developersRouter.post("/create", developersControllers.developersValidators, developersControllers.createDeveloper);
+developersRouter.post("/edit/:developerId", developersControllers.developersValidators, developersControllers.updateDeveloper);
+developersRouter.post("/delete/:developerId", developersControllers.deleteDeveloper);
 
 module.exports = developersRouter;
