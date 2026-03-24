bdc1dae4b5b5584a4b7bb59a80e5b4e348511d67
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2026-03-24 10:30:15 +0000

    feat: add GET, POST routes for create, edit, delete platforms

--- routes/platformsRouter.js
+++ routes/platformsRouter.js
@@ -3,6 +3,12 @@ const platformsRouter = Router();
 const platformsControllers = require("../controllers/platformsControllers");
 
 platformsRouter.get("/", platformsControllers.getAllPlatforms);
+platformsRouter.get("/create", platformsControllers.getCreatePlatform);
 platformsRouter.get("/:platformId", platformsControllers.getGamesByPlatformId);
+platformsRouter.get("/edit/:platformId", platformsControllers.getUpdatePlatform);
+
+platformsRouter.post("/create", platformsControllers.platformsValidators, platformsControllers.createPlatform);
+platformsRouter.post("/edit/:platformId", platformsControllers.platformsValidators, platformsControllers.updatePlatform);
+platformsRouter.post("/delete/:platformId", platformsControllers.deletePlatform);
 
 module.exports = platformsRouter;
